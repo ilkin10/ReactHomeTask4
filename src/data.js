d1aa6559bd67780d905e47b8dad2e43f034e4db7
ilkin <ilkin@MY_ASUS>
2023-07-23 11:08:37 +0400

    Done ReactHomeWork4

---
+++ src/data.js
@@ -0,0 +1,56 @@
+export var products = [
+  {
+    productIndex:1,
+    productName: "Nike Revolution",
+    productDescription:
+      "Nike Revolution 5 is a high-top version of the classic Nike Dunk sneaker, featuring a padded collar for added support and comfort.",
+    productPrice: 45,
+    productImage:
+      "https://static.nike.com/a/images/t_default/qwqfyddzikcgc4ozwigp/revolution-5-mens-road-running-shoes-ZXqS6C.png",
+  },
+  {
+    productIndex:2,
+    productName: "Nike Tanjun",
+    productDescription:
+      "Nike Tanjun is a high-top version of the classic Nike Dunk sneaker, featuring a padded collar for added support and comfort.",
+    productPrice: 65,
+    productImage:
+      "https://static.nike.com/a/images/t_default/60254be1-2776-4426-a8db-3f1df6ad0f6c/tanjun-mens-shoes-jQ3z1W.png",
+  },
+  {
+    productIndex:3,
+    productName: "Nike DBreak",
+    productDescription:
+      "Nike DBreak is a high-top version of the classic Nike Dunk sneaker, featuring a padded collar for added support and comfort.",
+    productPrice: 653,
+    productImage:
+      "https://static.nike.com/a/images/t_default/5493fdea-be62-4796-b3e9-092f56495845/daybreak-womens-shoes-qLjcF9.png",
+  },
+  {
+    productIndex:4,
+    productName: "Nike Wearallday",
+    productDescription:
+      "Nike Wearallday is a high-top version of the classic Nike Dunk sneaker, featuring a padded collar for added support and comfort.",
+    productPrice: 34,
+    productImage:
+      "https://static.nike.com/a/images/t_default/11bd4de0-9bc3-4132-8e31-3cdbff732ec9/wearallday-shoe-PDB9Wd.png",
+  },
+  {
+    productIndex:5,
+    productName: "Nike Court",
+    productDescription:
+      "Nike Court Vision is a high-top version of the classic Nike Dunk sneaker, featuring a padded collar for added support and comfort.",
+    productPrice: 12,
+    productImage:
+      "https://static.nike.com/a/images/t_default/99528c30-cd75-4646-bfad-af571bf6ae6e/court-vision-mid-next-nature-shoes-VKCWFj.png",
+  },
+  {
+    productIndex:6,
+    productName: "Nike Air Max",
+    productDescription:
+      "Nike Court Vision is a high-top version of the classic Nike Dunk sneaker, featuring a padded collar for added support and comfort.",
+    productPrice: 23,
+    productImage:
+      "https://static.nike.com/a/images/t_default/e0b13bf1-9be8-4329-b40f-d28b6ae62e7d/air-max-pulse-mens-shoes-ShS3tL.png",
+  },
+];
